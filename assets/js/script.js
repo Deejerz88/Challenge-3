4fbf62f866b56b0e3b50afb32a32d99814053736
Deejerz88 <djmcmillan93@gmail.com>
2022-06-11 13:45:43 -0400

    fixed password copy propagation duplicating event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-  passwordText.addEventListener("click", () => {
+}
+
+passwordText.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (passwordText.value !== "") {
     passwordText.select();
     navigator.clipboard.writeText(passwordText.value);
     alert("Copied password to clipboard");
-  });
-}
+  }
+});
 
 let charArr = [
   "abcdefghijklmnopqrstuvwxyz",
@@ -29,7 +32,7 @@ function generatePassword() {
     "How many characters would you like to use (min: 8, max: 128)"
   );
   if (pwLen === null || pwLen === undefined) {
-    return "Your Secure Password";
+    return "";
   }
   pwLen = isNaN(pwLen) || pwLen === "" ? 0 : pwLen;
   if (pwLen < 8 || pwLen > 128 || isNaN(pwLen))
@@ -38,7 +41,7 @@ function generatePassword() {
         `${pwLen} character(s) entered. Please specify a length between 8 and 128 characters`
       );
       if (pwLen === null || pwLen === undefined) {
-        return "Your Secure Password";
+        return "";
       }
       pwLen = isNaN(pwLen) || pwLen === "" ? 0 : pwLen;
     } while (pwLen < 8 || pwLen > 128);
@@ -49,8 +52,7 @@ function generatePassword() {
     `Please specify the type of characters you would like to use.\n  1. lowercase\n  2. UPPERCASE\n  3. Special Characters\n  4. Numbers\nYou may enter multiple numbers (ex:134)`
   );
 
-  if (typeChoice === null || typeChoice === undefined)
-    return "Your Secure Password";
+  if (typeChoice === null || typeChoice === undefined) return "";
 
   typeChoice = typeChoice.replace(" ", "");
 
